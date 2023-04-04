497c9c216af1a63f9a05143cdad02ef6c2ec6c4a
Denor74 <den74@inbox.ru>
2023-04-04 16:27:25 +0500

    kuki

--- index.php
+++ index.php
@@ -3,8 +3,14 @@
 var_dump($_COOKIE);
 setcookie('login', 'admin', 0, '/');
 
-setcookie($name, $value, $ttl, $patch);
+//setcookie($name1, $value1, $ttl1, $patch1);
 // login $name - куки
 // admin $value - значение которое мы задаем
 // 0 $ttl - значение когда должен истечь срок существование куки, 0 по умолчанию и работает до закрытия файла или в секундах
 // / $patch - путь в адресной строке  - '/' доступен из всех директорий сайта
+
+$name = "Denis";
+$age = "49";
+setcookie("name", $name);
+setcookie("age", $age, time() +3600); // срок действия 1 час
+echo "Куки установлены";
